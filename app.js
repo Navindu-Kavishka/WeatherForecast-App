b6200b2e39bb58b52c4c91e3ca58ae5a02a0f01c
Navindu Kavishka <navindukavishka2001@gmail.com>
2024-03-14 19:05:37 +0530

    time & date

--- app.js
+++ app.js
@@ -1,4 +1,44 @@
 
+const timeE1 = document.getElementById("time");
+const dateE1 = document.getElementById("date");
+
+
+const days =['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Satureday'];
+const months=['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+const API_KEY='ac3dfa207a7a62f68fef2f066e07e25e';
+
+setInterval(()=>{
+    const time = new Date();
+    const month= time.getMonth();
+    const date= time.getDate();
+    const day= time.getDay();
+    const hour= time.getHours();
+    const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+    const minutes = time.getMinutes();
+    const ampm = hour >= 12? "PM" : "AM";
+
+
+  timeE1.innerHTML = hoursIn12HrFormat + ':' +minutes+' ' +`<span id="am-pm">${ampm}</span>`
+
+  dateE1.innerHTML = days[day] + ', ' +date+' ' +months[month]
+}, 1000);
+
+getWeatherData();
+function getWeatherData () {
+    navigator.geolocation.getCurrentPosition((success)=> {
+        console.log(success);
+
+        let {latitude, longitude} = success.coords;
+
+        fetch (`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&appid=${API_KEY}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+          
+        })
+    })
+}
 
 
 
@@ -28,4 +68,13 @@ document.getElementById("searchBtn").addEventListener("click",()=>{
         document.getElementById("sunrise").innerHTML=data.current.sunrise;
         document.getElementById("sunset").innerHTML=data.current.sunset;
     })
+
+
+
+    // 5 days
+
+
+    
+
+
 })
